cd4bec18ff7d1f9a1b56bbbf2148569241c0cd56
Giovane Barcelos <giovanebarcelos@gmail.com>
2025-09-03 20:41:06 -0300

    feat: assinaturas da Calculadora

---
+++ src/main/java/calculadora/Calculadora.java
@@ -0,0 +1,23 @@
+package calculadora;
+
+public class Calculadora {
+    int somar(int num1, int num2){
+        return 0;
+    }
+
+    int subtrair(int num1, int num2){
+        return 0;
+    }
+
+    int multiplicar(int num1, int num2){
+        return 0;
+    }
+
+    int dividir(int num1, int num2){
+        return 0;
+    }
+
+    int fatorial(int num){
+        return 0;
+    }
+}
